2da2f5c01451b4436da98f10f86d17528f377a93
mohammedabdelaal <medo.elaal@gmail.com>
2023-02-04 02:04:08 +0200

    changed to require

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-import passport from 'passport';
-import GoogleStrategy from 'passport-google-oauth20/lib/strategy';
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const express = require('express');
 
 const app = express();
@@ -7,13 +7,13 @@ const app = express();
 const GOOGLE_CLIENT_ID =
   '611034003535-6drm0lika7ustglkbki5qc4c7ipss4eq.apps.googleusercontent.com';
 
-passport.use(
-  new GoogleStrategy({}, function verify(
-    accessToken,
-    refreshToken,
-    profile,
-    cb
-  ) {})
-);
+// passport.use(
+//   new GoogleStrategy({}, function verify(
+//     accessToken,
+//     refreshToken,
+//     profile,
+//     cb
+//   ) {})
+// );
 
 app.listen(5000);
